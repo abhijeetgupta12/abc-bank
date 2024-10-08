1298b7c350523f0b356c57c02e8900e2ccb4386b
abhijeetkumar.gupta <abhijeetkumar.gupta@PUNITP666594L.ad.infosys.com>
2024-10-08 12:50:16 +0530

    Added new account type as SUPER_SAVING and implemented the logic

--- src/main/java/com/abc/Account.java
+++ src/main/java/com/abc/Account.java
@@ -8,6 +8,7 @@ public class Account {
     public static final int CHECKING = 0;
     public static final int SAVINGS = 1;
     public static final int MAXI_SAVINGS = 2;
+    public static final int SUPER_SAVINGS = 3;
 
     private final int accountType;
     public List<Transaction> transactions;
@@ -41,15 +42,21 @@ public void withdraw(double amount) {
                     return amount * 0.001;
                 else
                     return 1 + (amount-1000) * 0.002;
-//            case SUPER_SAVINGS:
-//                if (amount <= 4000)
-//                    return 20;
+           
             case MAXI_SAVINGS:
                 if (amount <= 1000)
                     return amount * 0.02;
                 if (amount <= 2000)
                     return 20 + (amount-1000) * 0.05;
                 return 70 + (amount-2000) * 0.1;
+                
+            case SUPER_SAVINGS:      //New case added for Super Savings account
+                if (amount <= 1000)
+                    return amount * 0.04;
+                if (amount <= 2000)
+                    return 40 + (amount-1000) * 0.05;
+                return 140 + (amount - 2000) * 0.12;
+                    
             default:
                 return amount * 0.001;
         }
