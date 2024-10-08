bf186709ec94713851dcf5bb19edc61c474ddb0c
abhijeetkumar.gupta <abhijeetkumar.gupta@PUNITP666594L.ad.infosys.com>
2024-10-08 14:36:25 +0530

    Added Unit test cases to cover Transaction class

--- src/test/java/com/abc/TransactionTest.java
+++ src/test/java/com/abc/TransactionTest.java
@@ -2,12 +2,44 @@ package com.abc;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import java.util.Date;
+
+import org.junit.Before;
+
 public class TransactionTest {
+	
+	private Transaction transaction;
+	private Date date;
+	
+	@Before
+	public void setUp() {
+		
+		transaction = new Transaction(50.0);
+		 date = DateProvider.getInstance().now();
+		
+	}
+	
     @Test
-    public void transaction() {
-        Transaction t = new Transaction(5);
-        assertTrue(t instanceof Transaction);
+    public void transactionInstanceTest() {
+        
+        assertTrue(transaction instanceof Transaction);
+    }
+    
+    @Test
+	public void transactionAmountTest() {
+    	
+    	assertEquals(50.0,transaction.amount,0);
+    }
+    
+    @Test
+    public void testCurrentDate() {
+    	
+    	assertEquals(date.getYear(),transaction.transactionDate.getYear());
+    	assertEquals(date.getMonth(),transaction.transactionDate.getMonth());
+    	assertEquals(date.getDay(),transaction.transactionDate.getDay());
+    	
     }
 }
